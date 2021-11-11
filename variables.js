49d163efdf7e365867fac02591e68b89d3ebec95
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-11 19:35:09 -0300

    Trabalhando com variáveis e constantes

---
+++ variables.js
@@ -0,0 +1,15 @@
+let birthYear = 1993;
+const myName = "Fábio";
+const birthCity = "Salvador"
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Lauro de Freitas";
+
+console.log(birthCity);
