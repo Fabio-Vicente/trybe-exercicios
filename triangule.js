1529996e6ada371e13690feee979d8d74af4dde3
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-11 19:38:56 -0300

    Efeutando operações aritméticas

---
+++ triangule.js
@@ -0,0 +1,13 @@
+let height = 8;
+const base = 5;
+
+console.log(base);
+console.log(height);
+
+const area = base*height;
+
+console.log(area);
+
+const perimeter = 2*base + 2*height;
+
+console.log(perimeter);
