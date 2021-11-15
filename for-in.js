14d62262a8b8cb8c61c9e881dfed201de0338fed
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-15 16:31:52 -0300

    For-in

---
+++ for-in.js
@@ -0,0 +1,22 @@
+//1
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+for (const key in names) {
+    console.log('Olá', names[key]);
+};
+
+//2
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (key in car) {
+  console.log(key, car[key]);
+}
