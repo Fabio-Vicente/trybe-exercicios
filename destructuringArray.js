dc3128e945e02f644233334bd952f0800b087c46
Fábio Vicente <fab10_lima@hotmail.com>
2022-01-13 15:10:57 -0300

    Utilizando o destructing em arrays

---
+++ destructuringArray.js
@@ -0,0 +1,47 @@
+/* const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+
+const firstCountry = arrayCountries[0];
+const secondCountry = arrayCountries[1];
+const thirdCountry = arrayCountries[2];
+const fourthCountry = arrayCountries[3];
+
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada */
+
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [greetings, logGreentings] = saudacoes;
+
+logGreentings(greetings);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[animal, bebida, comida] = [comida, animal, bebida];
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,,...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
