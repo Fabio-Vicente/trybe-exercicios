534f81cc4d5e45208f26ab4d5ce71ab3a2f4d7df
Fábio Vicente <fab10_lima@hotmail.com>
2022-01-23 12:51:08 -0300

    Exercício bônus

--- api.js
+++ api.js
@@ -1,12 +1,20 @@
-/* const fetch = require('node-fetch') */
-
-const fetchAssets = () =>
+/* const fetch = require('node-fetch')
+ */
+const fetchAssets = (convert = 1) =>
     fetch(`https://api.coincap.io/v2/assets`)
-        .then(response => response.json())
-        .then(data => document.getElementById('currencies').innerText = data.data
-            .reduce((currencies, { name: currencyName, symbol, priceUsd}, index) =>
-                index < 10 ?
-                    `${currencies}${currencyName} (${symbol}): ${parseFloat(priceUsd).toFixed(2)}\n`:
-                    currencies, ''));
+    .then(response => response.json())
+    .then(data => document.getElementById('currencies').innerText = data.data
+    .reduce((currencies, { name: currencyName, symbol, priceUsd}, index) =>
+        index < 10 ?
+            `${currencies}${currencyName} (${symbol}): ${parseFloat(priceUsd * convert).toFixed(2)}\n`:
+            currencies, ''));
 
-window.onload = fetchAssets();
+const convertBRL = () =>
+    fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/brl.json')
+    .then(response => response.json())
+    .then(data => data.brl);
+
+const fetchAssetsConverted = async () =>
+    fetchAssets(await convertBRL());
+
+window.onload = fetchAssetsConverted();
