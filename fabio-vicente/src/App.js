d00424dbec28b78f9c4eb15fa5a660d08f67a2d2
Fábio Vicente <fab10_lima@hotmail.com>
2022-01-27 09:48:04 -0300

    Criando o primeiro componente de classe

---
+++ fabio-vicente/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Me from './Component'
+
+function App() {
+  return <Me />;
+}
+
+export default App;
---
+++ fabio-vicente/src/Component.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class Me extends Component {
+    render() {
+        return (
+            <div className='me'>
+                <h1>Fábio Vicente</h1>
+                <p>https://www.linkedin.com/in/fabio-vicente/</p>
+            </div>
+        )
+    }
+}
+
+export default Me;
