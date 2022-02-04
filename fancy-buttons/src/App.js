6d1bc9301ad3b09add0c36b3dd32f518dbb39b9a
Fábio Vicente <fab10_lima@hotmail.com>
2022-02-04 11:38:17 -0300

    Utilizando o `this` e realizando o `bind` dos métodos

--- fancy-buttons/src/App.js
+++ fancy-buttons/src/App.js
@@ -5,28 +5,38 @@ import './App.css';
 não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no botão!')
-}
 
-function handleClick2() {
-  console.log('Clicou no botão 2!')
-}
-
-function handleClick3() {
-  console.log('Clicou no botão 3!')
-}
 
 class App extends React.Component {
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
+
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  handleClick() {
+    console.log('Clicou no botão!')
+  }
+  
+  handleClick2() {
+    console.log('Clicou no botão 2!')
+  }
+  
+  handleClick3() {
+    console.log('Clicou no botão 3!')
+  }
+
   render() {
     return (
       <>
-        <button onClick={handleClick}>Meu botão</button>
-        <button onClick={handleClick2}>Meu botão 2</button>
-        <button onClick={handleClick3}>Meu botão 3</button>
+        <button onClick={this.handleClick}>Meu botão</button>
+        <button onClick={this.handleClick2}>Meu botão 2</button>
+        <button onClick={this.handleClick3}>Meu botão 3</button>
       </>
     )
   }
