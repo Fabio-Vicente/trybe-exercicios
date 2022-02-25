69b50a314041938e0693b4ec370b03b6d2bf83ca
Fábio Vicente <fab10_lima@hotmail.com>
2022-02-25 15:50:52 -0300

    Adicionando novas funcionalidades e realizando os seus teste

--- testes-react/src/App.test.js
+++ testes-react/src/App.test.js
@@ -11,15 +11,21 @@ test('Verificando se o botão e o campo email estão funcionando.', () => {
 
   const EMAIL_USER = 'email@email.com';
 
-  const textEmail = screen.getByTestId('id-email-user');
-  expect(textEmail).toBeInTheDocument();
-  expect(textEmail).toHaveTextContent('Valor:');
+  /* expect(textEmail).not.toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:'); */
 
   const btnSend = screen.getByTestId('id-send');
   const inputEmail = screen.getByLabelText('Email');
   userEvent.type(inputEmail, EMAIL_USER);
   userEvent.click(btnSend);
 
+  const textEmail = screen.getByTestId('id-email-user');
   expect(inputEmail).toHaveValue('');
   expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
+});
+
+test('Testando se não é exibida mensagem se não for passado um e-mail para o componente.', () => {
+  render(<App />)
+  const isValidElement = screen.queryByRole('heading', { level: 3 });
+  expect(isValidElement).not.toBeInTheDocument();
 });
